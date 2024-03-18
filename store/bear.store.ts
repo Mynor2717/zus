366acb65f46b9a083da64de471030520f83fb81e
node <ratafak15@gmail.com>
2024-03-18 16:20:34 -0400

    usando zustand carpetas

--- store/bear.store.ts
+++ store/bear.store.ts
@@ -2,8 +2,10 @@ import { create } from 'zustand';
 
 interface BearState {
     bears: number; 
+    increasePopulation: () => void;
 }
 
 export const useBearsStore = create<BearState>()((set) => ({
-    bears: 0
+    bears: 0,
+    increasePopulation: () => set((state) => ({bears: state.bears + 1})),
 }));
